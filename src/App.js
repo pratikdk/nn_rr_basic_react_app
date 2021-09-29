f1466570049b7f32c75e4a5a5a3c128b05679d79
pratikdk <pratik.deoolwadikar@gmail.com>
2021-09-29 15:50:35 +0530

    added a list prop render the list in child component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import Ninjas from "./Ninjas";
 
 class App extends React.Component {
+  state = {
+    ninjas : [
+      {name: "Ryu", age:"30", belt:"black", id: 1},
+      {name: "Yoshi", age:"20", belt:"White", id: 2},
+      {name: "Jane", age:"25", belt:"Green", id: 3},
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React App!</h1>
         <p>Welcome :0</p>
-        <Ninjas name="Ryu" age="30" belt="black"/>
-        <Ninjas name="Yoshi" age="20" belt="White"/>
+        <Ninjas ninjas={ this.state.ninjas }/>
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,12 +2,19 @@ import React from "react";
 
 class Ninjas extends React.Component {
     render() {
-        const {name, age, belt} = this.props;
+        const { ninjas } = this.props;
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div> 
+                </div>
+            )
+        })
         return (
-            <div className="ninja">
-                <div>Name: {this.props.name}</div>
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div> 
+            <div className="ninja-list">
+                { ninjaList }
             </div>
         );
     }
