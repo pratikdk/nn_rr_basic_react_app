cc30abc80287d892f10d22be5936927fb835d6ff
pratikdk <pratik.deoolwadikar@gmail.com>
2021-09-29 16:30:22 +0530

    added conditional rendering

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,20 +1,53 @@
 
 const Ninjas = ({ninjas}) => {
-    // const { ninjas } = props;
+    // // const { ninjas } = props;
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: {ninja.name}</div>
+    //                 <div>Age: {ninja.age}</div>
+    //                 <div>Belt: {ninja.belt}</div> 
+    //             </div>
+    //         );
+    //     }
+    //     else {
+    //         return null;
+    //     }
+    // })
+
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ninja.age > 20 ? (
             <div className="ninja" key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div> 
             </div>
-        )
+        ) : null;
     })
+
     return (
         <div className="ninja-list">
             { ninjaList }
         </div>
     );
+
+    // return (
+    //     <div className="ninja-list">
+    //         { 
+    //             ninjas.map(ninja => {
+    //                 return ninja.age > 20 ? (
+    //                     <div className="ninja" key={ninja.id}>
+    //                         <div>Name: {ninja.name}</div>
+    //                         <div>Age: {ninja.age}</div>
+    //                         <div>Belt: {ninja.belt}</div> 
+    //                     </div>
+    //                 ) : null;
+    //             })
+    //         }
+    //     </div>
+    // );
+
 
 }
 
