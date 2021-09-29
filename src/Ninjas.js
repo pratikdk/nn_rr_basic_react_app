0bda98937ffe865ccff3c279e6e111fa4e5913e1
pratikdk <pratik.deoolwadikar@gmail.com>
2021-09-29 15:09:34 +0530

    nested ninjas class component into app class component

---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Ninjas extends React.Component {
+    render() {
+        return (
+            <div className="ninja">
+                <div>Name: Ryu</div>
+                <div>Age: 30</div>
+                <div>Belt: Black</div> 
+            </div>
+        );
+    }
+}
+
+export default Ninjas;
