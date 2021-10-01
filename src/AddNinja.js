2773d7a5d17ea941679a6461428b08af7356c0f0
pratikdk <pratik.deoolwadikar@gmail.com>
2021-10-02 01:39:20 +0530

    added form, along with input and submit handler

---
+++ src/AddNinja.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class AddNinja extends React.Component {
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+    submitHandler = (e) => {
+        e.preventDefault();
+        console.log("Current State", this.state);
+    }
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.id] : e.target.value
+        });
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submitHandler}>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" onChange={this.changeHandler}/>
+                    <label htmlFor="age">Age:</label>
+                    <input type="text" id="age" onChange={this.changeHandler}/>
+                    <label htmlFor="belt">Belt:</label>
+                    <input type="text" id="belt" onChange={this.changeHandler}/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
 
 class App extends React.Component {
   state = {
@@ -15,6 +16,7 @@ class App extends React.Component {
         <h1>My first React App!</h1>
         <p>Welcome :0</p>
         <Ninjas ninjas={ this.state.ninjas }/>
+        <AddNinja />
       </div>
     );
   }
